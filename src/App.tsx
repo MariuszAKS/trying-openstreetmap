c320890447847b6f115b447416347f2ddeef0d3f
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-02 18:15:02 +0100

    Added geolocation via button

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,31 @@ import Map from './assets/components/Map'
 import './App.css'
 
 function App() {
+  const [userLocation, setUserLocation] = useState<GeolocationCoordinates | null>(null)
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation(position.coords)
+        },
+        (error) => {
+          alert('Couldn\'t retrieve user\'s location: ' + error)
+        }
+      )
+    }
+    else {
+      alert('Geolocation not supported in this browser.')
+    }
+  }
+
   return (
     <>
       <h1>Map app based on OpenStreetMap</h1>
-        <div className='MapContainer'>
-          <Map></Map>
-        </div>
+      <div className='MapContainer'>
+        <Map userLocation={userLocation}></Map>
+      </div>
+      <button onClick={getUserLocation}>Get user location</button>
     </>
   )
 }
