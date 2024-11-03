e095986ba0a0f9d1a5a04a51a96edede78bce072
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-03 16:09:45 +0100

    Added restrictions on dragging and zoom (centered on SGGW)

--- src/assets/components/Map.tsx
+++ src/assets/components/Map.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap, LayerGroup } from 'react-leaflet'
 
 export const Map = ({ userLocation }: {
   userLocation: GeolocationCoordinates | null
 }) => {
+
+  const sw = L.latLng(52.15656, 21.03624)
+  const ne = L.latLng(52.16740, 21.05596)
   
   return (
-    <MapContainer center={[52.24, 21.01]} zoom={10} scrollWheelZoom={true}>
+    <MapContainer
+      center={[52.16256, 21.04219]}
+      maxBounds={L.latLngBounds(sw, ne)}
+      zoom={16}
+      minZoom={16}
+      maxZoom={19}
+    >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {userLocation &&
         <Marker position={[userLocation.latitude, userLocation.longitude]}>
