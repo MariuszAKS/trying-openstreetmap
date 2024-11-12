baf4f9ee076b94f3525908e618d32fc63465a752
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-12 13:16:35 +0100

    Added api call and drawing route

--- src/assets/components/MapSGGW.tsx
+++ src/assets/components/MapSGGW.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import L from 'leaflet'
-import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents, LayerGroup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents, Polyline } from 'react-leaflet'
 import 'leaflet-routing-machine'
 // import './MapSGGW.css'
 
@@ -51,16 +51,36 @@ export const MapSGGW = ({ userLocation, markerType }: {
 }) => {
   const [location, setLocation] = useState<L.LatLng | null>(null)
   const [destination, setDestination] = useState<L.LatLng | null>(null)
+  const [route, setRoute] = useState<[L.LatLng, L.LatLng][]>([])
 
   const sw = L.latLng(52.15656, 21.03624)
   const ne = L.latLng(52.16740, 21.05596)
 
+  useEffect(() => {
+    if (location && destination) {
+      const newRoute: [L.LatLng, L.LatLng][] = []
+
+      fetch(`http://router.project-osrm.org/route/v1/foot/${location.lng},${location.lat};${destination.lng},${destination.lat}?geometries=geojson`)
+        .then(response => response.json())
+        .then(data => {
+          const coordinates: [number, number][] = data['routes'][0]['geometry']['coordinates']
+          coordinates.slice(0, -1).map((_, i) => {
+            const from = new L.LatLng(coordinates[i][1], coordinates[i][0])
+            const to = new L.LatLng(coordinates[i + 1][1], coordinates[i + 1][0])
+            newRoute.push([from, to])
+          })
+          setRoute(newRoute)
+        })
+        .catch(error => console.log(error))
+    }
+  }, [location, destination])
+
   return (
     <MapContainer
       center={[52.16256, 21.04219]}
-      maxBounds={L.latLngBounds(sw, ne)}
+      // maxBounds={L.latLngBounds(sw, ne)}
       zoom={16}
-      minZoom={16}
+      // minZoom={16}
       maxZoom={18}
     >
       <TileLayer
@@ -72,7 +92,11 @@ export const MapSGGW = ({ userLocation, markerType }: {
       {destination && <Marker position={destination}><Popup>Destination</Popup></Marker>}
 
       <OnMapClick markerType={markerType} setLocation={setLocation} setDestination={setDestination}/>
-      <Routing location={userLocation} destination={destination}></Routing>
+      {/* <Routing location={userLocation} destination={destination}></Routing> */}
+
+      {route.length > 0 &&
+        <Polyline positions={route}/>
+      }
 
       {userLocation &&
         <Marker position={userLocation}>
