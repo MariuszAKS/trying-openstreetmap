a86feaa5c6e0caa66ebb10aa5b38fdd2b5ca1a8f
MariuszAKS <mariusz-k-sikora@o2.pl>
2024-11-12 20:09:20 +0100

    Correctly recovering fetched data, note: fetch to api from documentation commented, still not working (says mode: driving, even thou added: foot), using request to demo project (this one works)

--- src/assets/components/MapSGGW.tsx
+++ src/assets/components/MapSGGW.tsx
@@ -4,30 +4,30 @@ import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents, Polyline 
 import 'leaflet-routing-machine'
 // import './MapSGGW.css'
 
-const Routing = ({ location, destination }: {
-  location: L.LatLng | null
-  destination: L.LatLng | null
-}) => {
-  const map = useMap()
+// const Routing = ({ location, destination }: {
+//   location: L.LatLng | null
+//   destination: L.LatLng | null
+// }) => {
+//   const map = useMap()
   
-  useEffect(() => {
-    if (!map) return
+//   useEffect(() => {
+//     if (!map) return
 
-    if (location === null || destination === null) return
+//     if (location === null || destination === null) return
 
-    L.Routing.control({
-      waypoints: [
-        L.latLng(location.lat, location.lng),
-        L.latLng(destination.lat, destination.lng)
-      ],
-      routeWhileDragging: true,
-      show: false,
-    }).addTo(map)
+//     L.Routing.control({
+//       waypoints: [
+//         L.latLng(location.lat, location.lng),
+//         L.latLng(destination.lat, destination.lng)
+//       ],
+//       routeWhileDragging: true,
+//       show: false,
+//     }).addTo(map)
     
-  }, [map, location, destination])
+//   }, [map, location, destination])
 
-  return null
-}
+//   return null
+// }
 
 function OnMapClick({ markerType, setLocation, setDestination }: {
   markerType: 'location' | 'destination'
@@ -36,6 +36,7 @@ function OnMapClick({ markerType, setLocation, setDestination }: {
 }) {
   useMapEvents({
     click: (e) => {
+      console.log(e.latlng)
       if (markerType === 'destination')
         setDestination(e.latlng)
       else setLocation(e.latlng)
@@ -59,16 +60,22 @@ export const MapSGGW = ({ userLocation, markerType }: {
   useEffect(() => {
     if (location && destination) {
       const newRoute: [L.LatLng, L.LatLng][] = []
+      const routePoints: L.LatLng[] = []
 
-      fetch(`http://router.project-osrm.org/route/v1/foot/${location.lng},${location.lat};${destination.lng},${destination.lat}?geometries=geojson`)
+      //fetch(`http://router.project-osrm.org/route/v1/bike/${location.lng},${location.lat};${destination.lng},${destination.lat}?steps=true`) // ?geometries=geojson
+      fetch(`https://routing.openstreetmap.de/routed-foot/route/v1/foot/${location.lng},${location.lat};${destination.lng},${destination.lat}?overview=false&steps=true`)
         .then(response => response.json())
         .then(data => {
-          const coordinates: [number, number][] = data['routes'][0]['geometry']['coordinates']
-          coordinates.slice(0, -1).map((_, i) => {
-            const from = new L.LatLng(coordinates[i][1], coordinates[i][0])
-            const to = new L.LatLng(coordinates[i + 1][1], coordinates[i + 1][0])
-            newRoute.push([from, to])
+          data['routes'][0]['legs'].map((legs: any) => {
+            legs['steps'].map((step: any) => {
+              step['intersections'].map((intersection: any) => {
+                const position: [number, number] = intersection['location']
+                routePoints.push(new L.LatLng(position[1], position[0]))
+              })
+            })
           })
+          routePoints.slice(0, -1).map((_, i) => {newRoute.push([routePoints[i], routePoints[i + 1]])})
+          console.log(newRoute)
           setRoute(newRoute)
         })
         .catch(error => console.log(error))
